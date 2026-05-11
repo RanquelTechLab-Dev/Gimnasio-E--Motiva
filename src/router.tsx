00c612490d9e8aa3cd0155c9e3eb5d3b5a7fdec3
RanquelTechLab-Dev <ranqueltechlab@gmail.com>
2026-05-11 14:01:05 -0300

    RAN-6 point router to shared placeholders

--- src/router.tsx
+++ src/router.tsx
@@ -3,21 +3,23 @@ import { RootLayout } from './shared/RootLayout';
 import { StudentLayout } from './shared/StudentLayout';
 import { AdminLayout } from './shared/AdminLayout';
 import { LoginPage } from './pages/LoginPage';
-import { StudentHomePage } from './pages/student/StudentHomePage';
-import { StudentCalendarPage } from './pages/student/StudentCalendarPage';
-import { StudentBookingsPage } from './pages/student/StudentBookingsPage';
-import { StudentPlanPage } from './pages/student/StudentPlanPage';
-import { StudentProfilePage } from './pages/student/StudentProfilePage';
-import { AdminHomePage } from './pages/admin/AdminHomePage';
-import { AdminStudentsPage } from './pages/admin/AdminStudentsPage';
-import { AdminPaymentsPage } from './pages/admin/AdminPaymentsPage';
-import { AdminCalendarPage } from './pages/admin/AdminCalendarPage';
-import { AdminAttendancePage } from './pages/admin/AdminAttendancePage';
-import { AdminPlansPage } from './pages/admin/AdminPlansPage';
-import { AdminEmailsPage } from './pages/admin/AdminEmailsPage';
-import { AdminStoragePage } from './pages/admin/AdminStoragePage';
-import { AdminSettingsPage } from './pages/admin/AdminSettingsPage';
-import { NotFoundPage } from './pages/NotFoundPage';
+import {
+  AdminAttendancePage,
+  AdminCalendarPage,
+  AdminEmailsPage,
+  AdminHomePage,
+  AdminPaymentsPage,
+  AdminPlansPage,
+  AdminSettingsPage,
+  AdminStoragePage,
+  AdminStudentsPage,
+  NotFoundPage,
+  StudentBookingsPage,
+  StudentCalendarPage,
+  StudentHomePage,
+  StudentPlanPage,
+  StudentProfilePage,
+} from './pages/placeholders';
 
 export const router = createBrowserRouter([
   {
